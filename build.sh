11fd5ac32ff55b34cfd4c42f47bec142ed39f836
obiwac <obiwac@gmail.com>
2021-12-21 21:35:36 +0100

    Added xterm source and also build system

--- build.sh
+++ build.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 set -e
 
+# remove previous build files
+
+rm -rf .build
 rm -rf .package
+
+# setup package
+
 aqua-manager --create --type custom
 cp -r package/* .package/
 aqua-manager --layout
+
+# compile xterm
+
+cp -r src/* .build/
+
+(
+	cd .build
+
+	./configure
+	make
+)
+
+cp .build/xterm .package/
+
+# package ZPK
+
 iar --pack .package --output package.zpk
--- package/entry.sh
+++ package/entry.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-bin/xterm $@
+chmod +x xterm
+./xterm -bg black -fg white $@
